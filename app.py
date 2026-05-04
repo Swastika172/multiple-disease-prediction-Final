ea3bd045d40b880fdddde77af81989d94bbb6387
Swastika Srivastava <swastiyashi1702@gmail.com>
2026-05-05 01:51:44 +0530

    Update app.py

--- app.py
+++ app.py
@@ -657,7 +657,7 @@ if selected == "Heart Disease Prediction":
     col1, col2, col3 = st.columns(3)
     with col1: age      = st.text_input("Age")
     with col2: sex      = st.text_input("Sex: 0 = Female; 1 = Male")
-    with col3: cp       = st.text_input("Chest Pain types")
+    with col3: cp       = st.text_input("Chest Pain types:0,1,2,3")
     with col1: trestbps = st.text_input("Resting Blood Pressure")
     with col2: chol     = st.text_input("Serum Cholestoral in mg/dl")
     with col3: fbs      = st.text_input("Fasting Blood Sugar > 120 mg/dl")
@@ -665,7 +665,7 @@ if selected == "Heart Disease Prediction":
     with col2: thalach  = st.text_input("Maximum Heart Rate achieved")
     with col3: exang    = st.text_input("Exercise Induced Angina")
     with col1: oldpeak  = st.text_input("ST depression induced by exercise")
-    with col2: slope    = st.text_input("Slope of the peak exercise ST segment")
+    with col2: slope    = st.text_input("Slope of the peak exercise ST segment:0=Downsloping(worst);1=Flat;2=Upsloping(best)")
     with col3: ca       = st.text_input("Major vessels colored by flourosopy")
     with col1: thal     = st.text_input("thal: 0 = normal; 1 = fixed defect; 2 = reversable defect")
 
